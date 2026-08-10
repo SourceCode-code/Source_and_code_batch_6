9f87f2f6f81ba23dc010d4c3519984cedd07bcd8
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-08-10 21:43:58 +0530

    added js demo

---
+++ Javascript/demo_1.js
@@ -0,0 +1,92 @@
+// when we create a js file we should always add a extension called .js 
+
+//JAVASCRIPT --> js is gobal web programing which is used to make your website interactive,
+//it allows deveopler or the user to create dynmaic web pages which will have a interactive response 
+
+
+// whatever webiste or web application we ahave are made up of 3 basic 
+
+// html --> provides structure   
+// css  --> provides desgin 
+// js  -->   provides logic 
+
+//
+
+// how to  run my first  code in js 
+
+// go to file 
+// right click on the file 
+// select option open in intergated terminal 
+
+// to print any thing in terminal 
+
+// used to display hello world 
+console.log("hello World") 
+
+// go to the terminal 
+// and now  enter the command --> node filename.js
+
+
+// COMMENTS --> Comments are the lines in js which are written in the code which are used to explain the code 
+// ( the comments are ignmored by the complier or IDE )
+
+
+// in js there are 2 types comments 
+
+// 1 single line comment  -->  (//) used for a single line or short comment
+
+//rftfyjgkuhilhgkjjjjjjjjjjjjjjjjjjjjjjj
+
+
+// 2 mutliple line comment --> /*        */  example of multiple line comments 
+
+/*
+this is 
+a mutliple 
+line comments 
+wedfvd
+wef
+*/
+
+// CODE QAULITY 
+
+// 1 uses comments to explain complex logic or algorithms
+// 2 Avoid Abnoxious comments that do not add value 
+// 3 keep the comments short as needed 
+// 4 Keep the comment updated with the latest changes 
+// 5 comments should be consie and clear to understand 
+
+
+
+
+
+
+
+/*
+// function here is a keyword 
+and then i have created a function to print 
+function hello (){
+console.log("hello")
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+// task will be 
+// create a js file
+// print your full name 
+// write your introduction in comment  
+
+
+
+
