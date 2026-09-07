d89396cfd822c78f5e1c7aea3d7cb95092b8e5a7
AnkitRandive8485 <ankitrandive.work@gmail.com>
2026-09-07 23:18:19 +0530

    02_assignment

---
+++ JAVASCRIPT/ASSIGNMENT/02_ASSIGNMENT.js
@@ -0,0 +1,38 @@
+// Print My name
+console.log("Ankit Randive")
+//My name is Ankit Randive
+
+//welcome to javascript
+
+/* My name is Ankit Randive is a student of javascript. 
+I am learning javascript from 6th batch of source and code.
+ I am very happy to learn javascript from source and code. 
+ I am very thankful to source and code for providing me 
+ this opportunity to learn javascript. I am very excited to learn javascript. 
+  I am very happy to learn javascript from source and code. 
+  I am very thankful to source and code for providing me this opportunity to learn javascript.
+*/
+
+let number_two = 2
+let number_three = 3
+
+console.log(number_two)
+console.log(number_three)
+
+number_two = 200
+number_three = 400
+
+console.log(number_two)
+console.log(number_three)
+
+let first_name = "Ankit"
+let last_name = "Randive"
+let marital_status = "Married"
+let country = "India"
+let age = 29
+
+console.log(first_name)
+console.log(last_name)
+console.log(marital_status)
+console.log(country)
+console.log(age)
