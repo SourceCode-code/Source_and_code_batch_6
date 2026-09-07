0ec6bd0465493396bfa6179051c36871730d04a6
shivani <shvanikshirsagar2204@gmail.com>
2026-09-08 01:45:17 +0530

    js 1st assignment

---
+++ my test/01_shivaniassignment.js
@@ -0,0 +1,23 @@
+//comment
+
+//my name is shivani kshirsagar
+
+
+ /*good morning,
+ i am shivani kshirsagar
+ i completed my degree in Bamu univercity
+ */
+
+
+ //veriables
+
+ let myname = "shivani kshirsagar"
+ console.log(myname)
+
+ myname ="shivani sarkale"
+ console.log(myname)
+
+ const mycountry = "india"
+ console.log(mycountry)
+
+ 
