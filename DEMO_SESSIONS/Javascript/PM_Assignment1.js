ceaf11ed34f41fcc84ddaecb7e07ab0c10e4dd43
parammore5050 <param.more5050@gmail.com>
2026-09-08 00:12:59 +0530

    updated changes

---
+++ DEMO_SESSIONS/Javascript/PM_Assignment1.js
@@ -0,0 +1,32 @@
+// Variable study case
+// Commonly used variable let, Const and var
+
+// Variable -->>> used to store the code value 
+// let -->> Value can be reassigned
+// const -->> Value cannot be reassigned
+// var -->> Value can be reassigned
+
+// let variable code
+
+let num_10=10
+let num_20=20
+
+// to show output in terminal
+console.log(num_10)
+console.log(num_20)
+
+// now update the value using declaring another let
+num_10= 100
+num_20= 200
+// to show latest change output in terminal
+console.log(num_10)
+console.log(num_20)
+
+// const variable code
+
+const name = "Parmeshwar";
+
+//name = "Rahul";  // ❌ Error
+console.log(name)
+
+// var varible code -->> same as let variable code
