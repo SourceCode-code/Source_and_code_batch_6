d420fa3a3d08f67113cea1abc34894e6e32da14e
Shitole, Vidya (EXTERN: Expleo GmbH) <vidya.shitole@expleogroup.com>
2026-09-08 15:38:37 +0530

    added file for JS assignment1

---
+++ JS_Ass1_vidya.js
@@ -0,0 +1,21 @@
+//JavaScript Lecture 1
+//What I learn in Today's Lecture are listed below
+
+/* 1. How to use comment-single line and multiline 
+   2. What is "Variable" and it's types [let, var, const]
+   3. How to declaration variables
+   4. Assignments for the day on Variable declaration
+*/
+
+
+//1. Variable declararion for "Let"
+let num1 = 10          //used to delacre a vlaue that can be updated in the future
+let num2 = 20
+console.log(num1+num2)
+num1 = 5               //reassigning value is possible 
+console.log(num1+num2)
+
+//2. Variable declararion for "const"
+const marks = 78.25     //used to delcare a constant variable
+console.log(marks)
+//marks = 80            //cannot reassign value- will through an error
