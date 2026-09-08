6eeb4824173c79bfd0db1c8313de89b1994ad893
sachinnagare <sachin.nagare3@gmail.com>
2026-09-08 22:37:00 +0530

    First assignment of print

---
+++ JAVASCRIPT/ASSIGNMENT/01_Sachin_Assignment.js
@@ -0,0 +1,25 @@
+let myName='Sachin'
+console.log(myName)
+// comments can make code readable
+// Welcome to JavaScript
+/* comments can make 
+code readable, 
+easy to reuse and informative
+*/
+let firstVariable
+let secondVariable,thirdVariable,fourthVariable
+
+let number_ten=10
+let number_five=5
+let number_fifty=50
+let number_four=4
+
+let firstName='sachin', lastName='Nagare', marritalStatus='Married',age=30,country="India"
+
+let myAge=30
+let yourAge=25
+console.log(myAge,yourAge)
+
+myAge=35
+yourAge=26
+console.log(myAge,yourAge)
