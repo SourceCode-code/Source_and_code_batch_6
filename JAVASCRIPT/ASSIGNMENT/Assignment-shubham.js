80c8114bef491eb83ad5a7f7c8aeb6920d625ecf
Shubham <bhopaleshubham47@gmail.com>
2026-09-08 19:03:06 +0530

    javascript first assignment -Shubham

---
+++ JAVASCRIPT/ASSIGNMENT/Assignment-shubham.js
@@ -0,0 +1,13 @@
+//printing my name
+console.log('Shubham Bhopale');
+
+//printing first name and last name using variables 
+let firstName = 'Shubham';
+let lastName = 'Bhopale';
+console.log(firstName + ' ' + lastName);
+
+// Declare  variables without assigning values
+let maritalStatus;
+maritalStatus = 'Single';
+let country ;
+country = 'India';
