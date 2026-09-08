585702932e63cc7785302588795f08f77cb1b105
RahulShinde1994 <rjshinde994@gmail.com>
2026-09-08 16:28:41 +0530

    Added assignment code

---
+++ GIT-GITHUB/Assignmentrahulshinde.js
@@ -0,0 +1,24 @@
+//Hi I am Rahul Shinde.
+
+//This my Assignment of 07-09-2026
+
+console.log("hello World") 
+
+
+let num1 = 100
+
+console.log(num1)
+
+num1 = 200
+
+console.log(num1)
+
+const num2 = 300
+
+console.log(num2)   
+
+//const num2 = 400
+
+var name = "Rahul Shinde"
+
+console.log(name)
