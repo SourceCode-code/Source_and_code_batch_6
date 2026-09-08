3dc010968dfa5be3a6f155b1b15d0365e576278e
RutujaVhanmane <rutujavhanmane.work@gmail.com>
2026-09-08 15:29:31 +0530

    Add JavaScript assignment

---
+++ JAVASCRIPT/ASSIGNMENT/Rutuja_Assignment.js
@@ -0,0 +1,44 @@
+// Q1 - Print your name in the terminal
+console.log("Rutuja Vhanmane");
+
+// comments can make code readable
+
+// Welcome to JavaScript
+
+
+// Declare variables in multiple lines
+let firstName = "Rutuja";
+let lastName = "Vhanmane";
+let maritalStatus = "Single";
+let country = "India";
+let age = 25;
+
+
+// Declare variables in a single line
+let firstName2 = "Rutuja", lastName2 = "Vhanmane", maritalStatus2 = "Single", country2 = "India", age2 = 22;
+
+
+// Declare myAge and yourAge
+let myAge = 22;
+let yourAge = 25;
+
+console.log("My age:", myAge);
+console.log("Your age:", yourAge);
+
+
+// comments can make code readable, easy to reuse and informative
+
+
+// Declare four variables without assigning values
+let variable1;
+let variable2;
+let variable3;
+let variable4;
+
+
+// Declare four variables with assigned values
+let number = 10;
+let city = "Mumbai";
+let isStudent = true;
+let marks = 85;
+// JavaScript assignment completed
