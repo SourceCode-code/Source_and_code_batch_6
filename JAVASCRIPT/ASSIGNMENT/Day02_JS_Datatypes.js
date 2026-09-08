c72ee8dc6bf4e15118681a44c100b9354725a372
pawarakshay136 <pawarakshay136@gmail.com>
2026-09-08 23:18:20 +0530

    JavaScript Data Types Practice - Day 02 - 08 September 2026, Tuesday

---
+++ JAVASCRIPT/ASSIGNMENT/Day02_JS_Datatypes.js
@@ -0,0 +1,44 @@
+// Day02: JavaScript Data Types  08th September 2026, Tuesday
+
+
+// Data Types
+// IN JS THERE TWO MAJOR CATAGORY DATA 
+
+// 1. Primitive Data Types - data types which are simple in nature and are non changeable (immutable) 
+// 2. Non-Primitive Data Types - the datatype which are complex in nature and change be change accroding to requrimnet 
+
+// Primitive Data Types:
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+
+// Non-Primitive Data Types:
+// 1. Array
+// 2. Object
+
+
+// Creating variables with different data types
+
+let Number_24 = 24;
+
+let Name = "Akshay Pawar";
+
+let value = true;
+
+let token;
+
+let x1 = null; 
+
+// Checking data type using typeof
+
+console.log("Data type of Number_24 is:- "+typeof Number_24)
+
+console.log("Data type of Name is:- "+typeof Name)
+
+console.log("Data type of value is:- "+typeof value)
+
+console.log("Data Type of token is:- "+ typeof token)
+
+console.log("Data Type of x1 is:- " + typeof x1)
