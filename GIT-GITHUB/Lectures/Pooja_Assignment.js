c2778393a5aa1a55792cbbbedaded2e37ed564fa
poojadombe <poojadombe.work@gmail.com>
2026-09-08 17:29:15 +0530

    09-sep-assignment

---
+++ GIT-GITHUB/Lectures/Pooja_Assignment.js
@@ -0,0 +1,47 @@
+// Name - Pooja Hrishikesh Dombe
+
+/* 
+Introduction
+
+I have 4.5+ years of overall professional experience, including 2.5+ years of experience in Procurement 
+and the P2P domain.
+
+I have worked on the end-to-end cycle, handling business requirements, resolving 
+process-related issues, and contributing to process improvements y streamlining activities.
+
+Over time, I developed a strong interest in the technical and quality aspects of applications, 
+which motivated me to pursue a career in Software Testing.
+
+I have been developing my QA skills through structured courses, self-learning, and hands-on practice.
+I have knowledge of Git and Manual Testing concepts, and I am currently learning JavaScript, followed 
+by Playwright for automation testing.
+
+My previous professional experience has given me strong skills in requirement analysis, problem-solving, 
+process understanding, attention to detail, stakeholder communication, and identifying process gaps. 
+I believe these skills provide a strong foundation for Software Testing.
+
+I am now looking for an opportunity where I can apply these skills and grow as a Software Quality 
+Engineer.
+
+*/
+
+
+//Veriables - 1.Let 
+
+let testName = loginPage
+console.log("testName");
+
+// updateing the testNAme 
+
+testName = seachPage
+console.log("testName");
+
+//Veriables - 2.const
+
+const testId = TC_LOGIN_001
+console.log("testId");
+
+// updateing the testId
+
+testId = TC_LOGIN_002
+console.log("testId");
