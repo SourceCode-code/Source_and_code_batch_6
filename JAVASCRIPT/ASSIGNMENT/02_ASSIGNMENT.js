bdc4c33b1f17512854ce9e56ac3da4a0b4e9592f
AnkitRandive8485 <ankitrandive.work@gmail.com>
2026-09-08 20:02:43 +0530

    Assignment

---
+++ JAVASCRIPT/ASSIGNMENT/02_ASSIGNMENT.js
@@ -0,0 +1,25 @@
+// Print my name 
+console.log("Roshani Randive")
+
+// welcome to JavaScript
+
+/* My name is Roshani Randive. I am learing javascript. I am enjoying it very much. 
+I am excited to learn more about javascript and its features. 
+
+*/
+
+let number_one = 1
+let number_two = 2
+
+console.log("number_one")
+console.log("number_two")
+
+
+let number_one = 100
+let number_two = 200
+
+console.log("number_one")
+console.log("number_two")
+
+
+
