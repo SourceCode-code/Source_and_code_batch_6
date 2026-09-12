15ffc4d38d0ee85e6dcd8759211e9e12656c00b9
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-09-12 15:08:42 +0530

    UPDATES

---
+++ STUDENTS/Ankit Randive/JS_ASSIGNMENTS/02_Assignment.js
@@ -0,0 +1,54 @@
+// Print My name
+console.log("Ankit Randive")
+//My name is Ankit Randive
+
+//welcome to javascript
+
+/* My name is Ankit Randive is a student of javascript. 
+I am learning javascript from 6th batch of source and code.
+ I am very happy to learn javascript from source and code. 
+ I am very thankful to source and code for providing me 
+ this opportunity to learn javascript. I am very excited to learn javascript. 
+  I am very happy to learn javascript from source and code. 
+  I am very thankful to source and code for providing me this opportunity to learn javascript.
+*/
+
+let number_two = 2
+let number_three = 3
+
+console.log(number_two)
+console.log(number_three)
+
+number_two = 200
+number_three = 400
+
+console.log(number_two)
+console.log(number_three)
+
+let first_name = "Ankit"
+let last_name = "Randive"
+let marital_status = "Married"
+let country = "India"
+let age = 29
+
+console.log(first_name)
+console.log(last_name)
+console.log(marital_status)
+console.log(country)
+console.log(age)
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+let first_name1 = "Ankit", Last_name1 = "Randive", marital_status1 = "Married", country1 = "India", age1 = 29
+
+console.log(first_name1)
+console.log(Last_name1)
+console.log(marital_status1)
+console.log(country1)
+console.log(age1)
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the console.
+let myage = 29 
+let yourage = 30
+
+console.log(myage)
+console.log(yourage)
---
+++ STUDENTS/Sachin Nagare/JS_ASSIGNMENTS/02_Assignment.js
@@ -0,0 +1,39 @@
+let first_Number=10
+let myName="Sachin"
+let boo_Status=true
+let und
+let nothing=null
+console.log(first_Number,myName,boo_Status,und,nothing)
+
+//Q2
+       let city = "Pune"
+       let marks = 92.5
+       let isPass = true
+       let result
+       console.log(typeof city,typeof marks,typeof isPass, typeof result)
+ 
+//Q3
+      let x;
+       console.log(x)
+       console.log(typeof x)
+//Q4
+       let v1 = null
+       console.log(typeof v1)
+
+//Q9
+       let a = 10
+       let b = a
+       a = 20
+       console.log(a) //20
+       console.log(b) //10
+//Q10
+//undefined is a variable value did not set. variable value set as null 
+let myUndefine
+let myNull=null
+console.log(myUndefine,myNull)
+//Q13
+        let x1;
+        let y = null;
+        let z = "25";
+        console.log(typeof x1, typeof y, typeof z)
+//undefined , object,string
