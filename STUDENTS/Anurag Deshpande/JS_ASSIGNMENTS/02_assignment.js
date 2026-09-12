04c68f770767c00f2547e66cbd8df0e2795885d4
anuragdeshpandework <anuragdeshpande.work@gmail.com>
2026-09-12 15:23:36 +0530

    assignment2

---
+++ STUDENTS/Anurag Deshpande/JS_ASSIGNMENTS/02_assignment.js
@@ -0,0 +1,19 @@
+//Q1 --> Declare one variable for each primitive datatype (number, string, boolean, undefined, null)
+
+let number = 52;
+let string = "welcome to the JS"
+let boolean = true
+let uni = null 
+let undefined 
+
+
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof boolean);
+console.log(typeof uni);
+console.log(typeof undefined);
+
+console.log(typeof(number));
+
+
+
