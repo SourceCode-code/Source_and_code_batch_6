a04ea78e7baaa77d6c45bf168d5afb778832f8f9
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-09-15 22:11:01 +0530

    string methods

--- JAVASCRIPT/LECTURE/04_JS_String.js
+++ JAVASCRIPT/LECTURE/04_JS_String.js
@@ -85,3 +85,81 @@ let fullName_backtick = `my fullname is ' ${firstName} ${middleName} ${lastName}
 console.log(fullName_backtick)
 
 
+
+// NOTE : - 
+
+// STRING ARE IMMUTABLE IN JAVASCRIPT 
+
+//-------------
+/** 
+ * IT MEANS ONCE A STRING IS CREATED, THE CHARACTER IN IT CANNOT BE CHANGES OR MODIFIED 
+ * 
+ * IF ANY  METHOD THAT SEEMS TO MODIFY A STRING ACTUALLY , RETURNS A NEW STRING INSTEAD OF CHANGING THE ORGINAL  
+ */
+
+
+
+// METHOD OF STRING 
+
+// 1 toUpperCase()  -> THIS METHOD WILL CONVERT YOUR STRING INTO UPPERCASE( LETTERS/ALPHABATES )
+
+ let Meth_str_1 = "hello i am learning string method in js"
+
+ console.log(Meth_str_1.toUpperCase()) // output = HELLO I AM LEARNING STRING METHOD IN JS 
+
+ console.log(Meth_str_1)  // beacuse the original is not changed even after using a method instead it created a new strinfg 
+ // so it is called as immutable 
+
+ // 2 toLowerCase() -> THIS METHOD WILL CONVERT YOUR STRING INTO LOWERCASE ( LETTERS/ALPHABATES )
+
+ let Meth_str_2 = "HELLO I AM LEARNING STRING METHOD IN JS"
+
+ console.log(Meth_str_2.toLowerCase()) // hello i am learning string method in js
+
+
+//string contaction -> joining two or more string 
+
+//method concatenation -> 
+// when we call multiple method on same string or chaning multiple it called method chaning 
+
+//example
+
+let method_concat = "HELLO I AM LEARNING STRING METHOD IN JS"
+
+console.log(method_concat.toUpperCase().toLowerCase().toUpperCase().toLowerCase()) // this is method chaining
+
+// NOTE : - 
+// THIS WILL ONLY WORK WHEN THE OUTPUT OF FIRST METHOD IS A VALID INPUT TO SECOND METHOD 
+
+// EXAMPLE 
+// console.log(method_concat.toUpperCase().length.toLowerCase().toUpperCase().toLowerCase())
+
+// here error occures beacuse the output of  method_concat.toUpperCase().length is a number  
+// which is not a valid input toLowerCase()
+
+
+// 3. trim() --> this is used to remove the blank space form start and end of string 
+let method_Trim = " HELLO I AM LEARNING STRING METHOD IN JS "
+console.log(method_Trim)
+console.log(method_Trim.length)
+
+console.log(method_Trim.trim())
+console.log(method_Trim.trim().length)
+
+// 4 trimStart() -> this is used to remove the blank space form start 
+
+let method_TrimSTART = " HELLO I AM LEARNING STRING METHOD IN JS"
+console.log(method_TrimSTART)
+console.log(method_TrimSTART.length)
+
+console.log(method_TrimSTART.trimStart())
+console.log(method_TrimSTART.trimStart().length)
+
+// 5 trimEnd() ->this is used to remove the blank space form the end of string 
+
+let method_TrimEnd = "HELLO I AM LEARNING STRING METHOD IN JS "
+console.log(method_TrimEnd)
+console.log(method_TrimEnd.length)
+
+console.log(method_TrimEnd.trimEnd())
+console.log(method_TrimEnd.trimEnd().length)
