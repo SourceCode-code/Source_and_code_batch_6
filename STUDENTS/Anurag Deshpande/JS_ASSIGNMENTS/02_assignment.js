84137d808b1f1202143bfafc52edc1d8c1581fd4
deshpanu <anurag.deshpande@ikione.com>
2026-09-21 10:26:45 +0530

    added 2nd question

--- STUDENTS/Anurag Deshpande/JS_ASSIGNMENTS/02_assignment.js
+++ STUDENTS/Anurag Deshpande/JS_ASSIGNMENTS/02_assignment.js
@@ -1,3 +1,5 @@
+// ------------------- SECTION A : PRIMITIVE DATA TYPES -------------------
+
 //Q1 --> Declare one variable for each primitive datatype (number, string, boolean, undefined, null)
 
 let number = 52;
@@ -16,4 +18,13 @@ console.log(typeof undefined);
 console.log(typeof(number));
 
 
-
+//Q2 --> Write a code to check and print the datatype of the below variables using typeof operator
+let city = "Pune"
+let marks = 92.5
+let isPass = true
+let result
+//       HINT -> typeof is an operator, example -> console.log(typeof city)
+console.log(typeof city);
+console.log(typeof marks);
+console.log(typeof isPass);
+console.log(typeof result);
