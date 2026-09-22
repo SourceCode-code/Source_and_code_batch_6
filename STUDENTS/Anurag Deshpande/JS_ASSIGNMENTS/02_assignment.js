3509d54204b2f6231e0e60b7addbbb50faa908ad
Anurag Deshpande <anuragdeshpande.work@gmail.com>
2026-09-22 17:43:02 +0530

    added the next questions

--- STUDENTS/Anurag Deshpande/JS_ASSIGNMENTS/02_assignment.js
+++ STUDENTS/Anurag Deshpande/JS_ASSIGNMENTS/02_assignment.js
@@ -1,5 +1,6 @@
 // ------------------- SECTION A : PRIMITIVE DATA TYPES -------------------
 
+console.log("==================================");
 //Q1 --> Declare one variable for each primitive datatype (number, string, boolean, undefined, null)
 
 let number = 52;
@@ -17,7 +18,7 @@ console.log(typeof undefined);
 
 console.log(typeof(number));
 
-
+console.log("==================================");
 //Q2 --> Write a code to check and print the datatype of the below variables using typeof operator
 let city = "Pune"
 let marks = 92.5
@@ -29,8 +30,25 @@ console.log(typeof marks);
 console.log(typeof isPass);
 console.log(typeof result);
 
+console.log("==================================");
+//Q3 --> What is the output of the below code ? write the answer as a comment and then run to verify
+let x;
+console.log(x)
+console.log(typeof x)
+// js will not assigne any value to x so it will be undefined and the type of x will also be undefined
+
+console.log("==================================");
+//Q4 --> What is the output of the below code ? write the answer as a comment and then run to verify
+let v1 = null;
+console.log(typeof v1);
 
 
+// ------------------- SECTION B : PREDICT THE OUTPUT -------------------
 
-
-
+//Q9 --> Predict the output of the below code (write answer as comment, then run and verify)
+//       let a = 10
+//       let b = a
+//       a = 20
+//       console.log(a)
+//       console.log(b)
+//       HINT -> primitives are copied by VALUE
