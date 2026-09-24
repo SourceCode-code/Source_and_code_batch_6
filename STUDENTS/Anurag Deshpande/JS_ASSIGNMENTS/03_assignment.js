bc6f190e5fe6d6c6076c9eda9d34d59c824b2d30
Anurag Deshpande <anuragdeshpande.work@gmail.com>
2026-09-24 14:52:33 +0530

    added assignment 3

--- STUDENTS/Anurag Deshpande/JS_ASSIGNMENTS/03_assignment.js
+++ STUDENTS/Anurag Deshpande/JS_ASSIGNMENTS/03_assignment.js
@@ -1,4 +1,5 @@
 // ------------------- SECTION A : BASICS -------------------
+console.log("section A")
 
 //Q1 --> Declare one integer variable and one floating (decimal) variable of your choice.
 //       Print both values AND their datatypes using typeof.
@@ -40,5 +41,69 @@ console.log("Q4");
 //Q4 --> INTERVIEW QUESTION -> Math.round() and .toFixed() BOTH look like they "round".
 //============Math.round()==============
 /**
- * 1. Math.round() is a method of the math object 
+ * 1. Math.round() method is called using Math.round().
+ * 2. the number is rounded to the nearest integer 
+ * 3. returns a NUMBER 
  */
+
+//==============.toFixed()======================
+/**
+ * 1. it is called using number .toFixed()
+ * 2. the number is fixed number of decimal places 
+ * 3. returns a STRING 
+ */
+let num_1 = 10.6789
+console.log(Math.round(num_1));
+//OUTPUT : 11 
+console.log(typeof Math.round(num_1));
+//OUTPUT : NUMBER 
+
+console.log(num_1.toFixed(2));
+//OUTPUT : 10.68
+console.log(typeof num_1.toFixed(2));
+// OUTPUT : STRING 
+
+// ------------------- SECTION B : PREDICT THE OUTPUT -------------------
+console.log("section B")
+//Q5 --> Predict the output of the below code (write answer as comment, then run and verify)
+console.log(Math.floor(-4.2))   //OUTPUT : -5 
+console.log(Math.ceil(-4.8))    //OUTPUT : -4
+console.log(Math.round(-4.5))   //OUTPUT : -4
+
+//REASON :
+/**
+ * 1. Math.floor(-4.2) : -5 
+ * floor always goes to lower integer (towerds -infinity)
+ * 
+ * 2. Math.ceil(-4.8) : -4
+ * ceil alweay goes to higher integer (towerds +infinity)
+ * 
+ * 3. Math.round(-4.5) : -4
+ * for the negetive values round will rounds towerds to +infinity (towerds +infinity)
+ */
+
+console.log("================================================")
+//Q6 --> Predict the output of the below code (write answer as comment, then run and verify)
+console.log("Q6");
+let price_1 = 99.99
+console.log(Math.floor(price_1));
+//OUTPUT :99  rounds down 
+console.log(Math.ceil(price_1));
+//OUTPUT : 100  rounds up 
+console.log(Math.round(price_1));
+//OUTPUT :  100 rounds to nearest integer 
+console.log(price_1.toFixed(1));
+//OUTPUT : 100.0 formates numnber to 1 decimal place.
+// it returns as a string not a number 
+console.log(typeof price_1.toFixed(1));
+
+console.log("================================");
+//Q7 --> Predict the output of the below code (write answer as comment, then run and verify)
+console.log("Q7");
+
+let x = 10.658912355
+console.log(x.toFixed(2))   //OUTPUT : 10.66
+// it will reduce the value to 2 decimal places 
+console.log(x)  //OUTPUT : 10.658912355
+
+
