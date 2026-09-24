087ef3bc19c55458c822eb4dbe3cd7eb8e7ee061
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-09-24 21:46:36 +0530

    conditions

--- JAVASCRIPT/LECTURE/07_conditions.js
+++ JAVASCRIPT/LECTURE/07_conditions.js
@@ -91,7 +91,7 @@ else if (student_marks_1 > 70 && student_marks_1 <= 80) { console.log(` He has r
 
 else if (student_marks_1 >= 40 && student_marks_1 <= 70) { console.log(` He has recvied D grade `) }
 
-else if (student_marks_1 < 40 ) { console.log(` He has failed the exam `) }
+else if (student_marks_1 < 40) { console.log(` He has failed the exam `) }
 
 else { console.log("the student was not present for the exam") }
 
@@ -108,14 +108,155 @@ else { console.log("the student was not present for the exam") }
  */
 
 
-let season = "SUMMER"
+let season = "SUmmER"
 
-if(season.toLocaleLowerCase()==="summer"){console.log("carry cap and water bottle")}
+if (season.toLocaleLowerCase() === "summer") { console.log("carry cap and water bottle") }
 
-else if(season.toLocaleLowerCase()==="rainy"){console.log("carry raincoat /umberlla")}
+else if (season.toLocaleLowerCase() === "rainy") { console.log("carry raincoat /umberlla") }
 
-else if(season.toLocaleLowerCase()==="winter"){console.log("carry warm clothes/gloves")}
+else if (season.toLocaleLowerCase() === "winter") { console.log("carry warm clothes/gloves") }
 
-else{ console.log("enter correct input")}
+else { console.log("enter correct input") }
 
 
+
+// write program to check the person geneation
+
+
+let birthyear = 1995
+
+if (birthyear > 1980 && birthyear <= 1990) {
+    console.log("person belongs to boomer genartion")
+}
+else if (birthyear > 1991 && birthyear <= 2000) {
+    console.log("person is millieun genartion")
+}
+else if (birthyear > 2000) {
+    console.log("person belongs to generation z")
+}
+else { console.log("check your inputs ") }
+
+
+
+
+//Switch CASE -> this is latest addition in es6 ( and this most prefered by deveopler due to syntax clarity )
+
+
+/**
+ * syntax
+ * 
+ * 
+ * switch(parameter ){
+ * 
+ * case 1 :case 1 output
+ * case 2 :case 2 output 
+ * case 3 :case 3 output
+ *
+ * default : default output 
+ * 
+ * }
+ */
+
+// check if number is positive or negative 
+
+console.log("------------------switch case -------------------")
+let num_1 = 10
+
+
+switch (true) {
+
+    case num_1 > 0: console.log(`the given number ${num_1} is postive number`)
+        break
+    case num_1 < 0: console.log(`the given number ${num_1} is negative number`)
+        break
+    case num_1 === 0: console.log(`the given number is zero`)
+        break
+    default: console.log(` the given input is NAN`)
+}
+
+// in addition to swtich for every case we need a keyword which will end the case if condition met -->
+// that keyword break
+
+// break -> it will end the program when the case is met 
+
+
+// create a program to inform the employee about to work form office or home  or holiday 
+
+let day = "sun"
+
+let format_day = day.toLowerCase()
+
+
+switch (format_day) {
+    case "monday" || "mon": console.log("work form office")
+        break
+    case "tuesday" || "tue": console.log("work form office")
+        break
+    case "wednesday" || "wed": console.log("work form home")
+        break
+    case "thursday" || "thu": console.log("work form home")
+        break
+    case "friday" || "fri": console.log("work form home")
+        break
+    case "sat": console.log("week off")
+        break
+    case "sun": console.log("week off")
+        break
+    default: console.log("enter correct input")
+
+}
+
+/**
+ * 
+ * 
+let student_marks_1 = undefined
+
+if (student_marks_1 > 90) { console.log(` He has recvied A grade `) }
+else if (student_marks_1 > 80 && student_marks_1 <= 90) { console.log(` He has recvied B grade `) }
+else if (student_marks_1 > 70 && student_marks_1 <= 80) { console.log(` He has recvied C grade `) }
+else if (student_marks_1 >= 40 && student_marks_1 <= 70) { console.log(` He has recvied D grade `) }
+else if (student_marks_1 < 40) { console.log(` He has failed the exam `) }
+else { console.log("the student was not present for the exam") }
+
+ */
+
+let marks = 91
+
+switch (true) {
+    case marks > 90: console.log(` He has recvied A grade `)
+        break
+    case marks > 80 && marks <= 90: console.log(` He has recvied B grade `)
+        break
+    case marks > 70 && marks <= 80: console.log(` He has recvied C grade `)
+        break
+    case marks >= 40 && marks <= 70: console.log(` He has recvied D grade `)
+        break
+    case marks < 40: console.log(` He has failed the exam `)
+        break
+    default: console.log("the student was not present for the exam")
+
+}
+
+
+
+
+// optimzed example 
+let day_1 = "monday"
+
+let format_day_1 = day_1.toLowerCase()
+
+
+switch (format_day_1) {
+    case "monday" || "mon":
+    case "tuesday" || "tue": console.log("work form office")
+        break
+    case "wednesday" || "wed":
+    case "thursday" || "thu":
+    case "friday" || "fri": console.log("work form home")
+        break
+    case "sat":
+    case "sun": console.log("week off")
+        break
+    default: console.log("enter correct input")
+
+}
